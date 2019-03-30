6c8f049e868d3f316a88ce99b003db067f2321cb
Andrey Rudenko <andrey.rudenko@gmail.com>
2019-03-31 01:19:10 +0200

    Apply formatting

--- src/Entity/Genre.php
+++ src/Entity/Genre.php
@@ -9,70 +9,62 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\GenreRepository")
  */
-class Genre
-{
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+class Genre {
+  /**
+   * @ORM\Id()
+   * @ORM\GeneratedValue()
+   * @ORM\Column(type="integer")
+   */
+  private $id;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $name;
+  /**
+   * @ORM\Column(type="string", length=255, unique=true)
+   */
+  private $name;
 
-    /**
-     * @ORM\ManyToMany(targetEntity="App\Entity\Channel", inversedBy="genres")
-     */
-    private $channels;
+  /**
+   * @ORM\ManyToMany(targetEntity="App\Entity\Channel", inversedBy="genres")
+   */
+  private $channels;
 
-    public function __construct()
-    {
-        $this->channels = new ArrayCollection();
+  public function __construct() {
+    $this->channels = new ArrayCollection();
+  }
+
+  public function getId(): ?int {
+    return $this->id;
+  }
+
+  public function getName(): ?string {
+    return $this->name;
+  }
+
+  public function setName(string $name): self {
+    $this->name = $name;
+
+    return $this;
+  }
+
+  /**
+   * @return Collection|Channel[]
+   */
+  public function getChannels(): Collection {
+    return $this->channels;
+  }
+
+  public function addChannel(Channel $channel): self {
+    if (!$this->channels->contains($channel)) {
+      $this->channels[] = $channel;
     }
 
-    public function getId(): ?int
-    {
-        return $this->id;
+    return $this;
+  }
+
+  public function removeChannel(Channel $channel): self {
+    if ($this->channels->contains($channel)) {
+      $this->channels->removeElement($channel);
     }
 
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|Channel[]
-     */
-    public function getChannels(): Collection
-    {
-        return $this->channels;
-    }
-
-    public function addChannel(Channel $channel): self
-    {
-        if (!$this->channels->contains($channel)) {
-            $this->channels[] = $channel;
-        }
-
-        return $this;
-    }
-
-    public function removeChannel(Channel $channel): self
-    {
-        if ($this->channels->contains($channel)) {
-            $this->channels->removeElement($channel);
-        }
-
-        return $this;
-    }
+    return $this;
+  }
 }
